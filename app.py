552d0eb33826b9611847edacffdedf4ee840bec3
Jennifer Turliuk <kingstonlives@gmail.com>
2025-01-12 18:06:33 +0000

    added app,py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from climate_scale_up_dashboard import main
+
+# Set the app's page title and layout
+st.set_page_config(page_title="Climate Scale-Up Dashboard", layout="wide")
+
+# Entry point for the Streamlit app
+if __name__ == "__main__":
+    main()
